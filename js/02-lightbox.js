da4ef16a97c9299404e2c8f3eacd5d6becf0141f
Артем Гладуш <m1hommage5008@gmail.com>
2022-09-03 16:10:43 +0300

    addSimpleGalleryBox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
+const divRef = document.querySelector('.gallery');
+
+function createGalleryMrkup (items)  {
+	return items
+		.map((item) => 
+		`
+		<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+		`
+		).join("")
+};
+
+const addGalleryMarkup = createGalleryMrkup(galleryItems);
+
+divRef.insertAdjacentHTML('beforeend', addGalleryMarkup);
+
+divRef.addEventListener('click', onClickImage);
+
+function onClickImage(e) {
+	e.preventDefault();
+
+	if(e.target.nodeName !== "IMG") {
+		return;
+	}
+
+	var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250,});
+}
+
 console.log(galleryItems);
