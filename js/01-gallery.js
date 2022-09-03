a19bea1aee4b8ad70e78ca264c0a0bba0b7d2331
Артем Гладуш <m1hommage5008@gmail.com>
2022-09-03 14:52:47 +0300

    addGallaryBasicLightBox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const divRef = document.querySelector('.gallery');
+
+function createGalleryMrkup (items)  {
+	return items
+		.map((item) => 
+		`
+		<div class="gallery__item">
+			<a class="gallery__link" href="${item.original}">
+				<img
+					class="gallery__image"
+					src="${item.preview}"
+					data-source="${item.original}"
+					alt="${item.description}"
+					/>
+			</a>
+		</div>
+		`
+		).join("")
+};
+
+const addGalleryMarkup = createGalleryMrkup(galleryItems);
+
+divRef.insertAdjacentHTML('beforeend', addGalleryMarkup);
+
+divRef.addEventListener('click', onClickImage);
+
+function onClickImage(e) {
+	e.preventDefault();
+
+	if(e.target.nodeName !== "IMG") {
+		return;
+	}
+
+	const instance = basicLightbox.create(`
+		<img src="${e.target.dataset.source}" width="800" height="600">
+`);
+instance.show();
+
+divRef.addEventListener('keydown', (e) => {
+	if(e.code === 'Escape') {
+		instance.close();
+	};
+})
+}
+
+
+
+console.log(addGalleryMarkup);
 console.log(galleryItems);
